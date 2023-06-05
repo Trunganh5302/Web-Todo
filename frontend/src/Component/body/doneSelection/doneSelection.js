0705a987b9db51edd3a3d1cc6ca1c26dec80a3c8
TrungAnh <trunganh.edu.net@gmail.com>
2023-06-05 14:23:50 +0700

    basic add title section

---
+++ frontend/src/Component/body/doneSelection/doneSelection.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DoneSection() {
+  return (
+    <div>
+      <p>Section added</p>
+    </div>
+  );
+}
+
+export default DoneSection;
--- frontend/src/Component/body/selection/selection.js
+++ frontend/src/Component/body/selection/selection.js
@@ -1,22 +1,65 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faComment } from '@fortawesome/free-solid-svg-icons';
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import "../selection/selection.css";
+import doneSeclection from "../doneSelection/doneSelection";
 
 function Selection() {
-  const [showInput, setShowInput] = useState(false);
+  const [showInput, setShowInput] = useState(false); // Hiển thị input mặc định ban đầu là false
+  const [sectionName, setSectionName] = useState(""); // set Tên section ban đầu là rỗng
 
-  function handleClick() {
-    setShowInput(true)
+  function handleClick() { // Hàm hiển thị input khi người dùng nhập vào
+    setShowInput(true); // khi này setShowInput = true và gán true cho showInput = true
   }
+
+  function handleHide() { // hàm ẩn ô input khi người dùng click cancel
+    setShowInput(false); // khi này setShowInput = false và gán true cho showInput = false
+    setSectionName(""); // Tên section khi đó sẽ set lại từ đầu = " ", 
+                        // nhưng dữ liệu vẫn được lưu trong state dữ liệu ko mất cho tới khi reload
+  }
+
+  function handleChange(event) { // hàm thay đổi giá trị trong ô input
+    setSectionName(event.target.value);
+  }
+
+  function handleAddSection() { // hàm thêm section 
+    if (sectionName.trim() !== "") {
+      // Perform the action to add the section
+      console.log("Section Name:", sectionName);
+    }
+  }
+
   return (
     <>
-      <div>
-        <div className="icon-container" onClick={handleClick}>
-          <FontAwesomeIcon icon={faComment} />
-          <span>Text</span>
+      {showInput ? (
+        <div className="Action_add">
+          <input
+            type="text"
+            className="input_section"
+            placeholder="Name this section"
+            value={sectionName}
+            onChange={handleChange}
+          />
+          <div className="option">
+            <a
+              className={`btn_addSecion ${sectionName.trim() === "" ? "" : "active"}`} 
+              // kiểm tra từ khoảng trắng bằng trim(), nếu bằng rỗng thì ko có gì xảy ra,
+              // còn nếu nó khác rỗng thì set className = active
+              onClick={handleAddSection}
+            >
+              Add Section
+            </a>
+            <a className="btn_cancel" onClick={handleHide}>
+              Cancel
+            </a>
+          </div>
         </div>
-        {showInput && <input type="text" />}
-      </div>
+      ) : (
+        <div className="icon-container" onClick={handleClick}>
+          <FontAwesomeIcon icon={faPlus} style={{ color: "#b15daa" }} />
+          <span className="title_section">Add section</span>
+        </div>
+      )}
     </>
   );
 }
