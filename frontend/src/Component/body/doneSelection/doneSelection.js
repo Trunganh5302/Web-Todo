ba9c1d1a3275cda8d035a3505ac2a62e12a2c9b1
TrungAnh <trunganh.edu.net@gmail.com>
2023-06-07 21:53:43 +0700

    component box edit section

--- frontend/src/Component/body/doneSelection/doneSelection.js
+++ frontend/src/Component/body/doneSelection/doneSelection.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "../doneSelection/doneSelection.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPenToSquare,
+  faPlus,
+  faCopy,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
 
 function DoneSection({ sectionName }) {
+  const [showBox, setShowBox] = useState(false);
+
+  function handleToggleBox() {
+    setShowBox(!showBox);
+  }
+
   return (
     <div className="wrapperContainer">
       <div className="containerSection">
@@ -11,7 +22,28 @@ function DoneSection({ sectionName }) {
           <p className="titleSection">{sectionName}</p>
           <span className="numTask">1</span>
         </div>
-        <a className="SectionMenu">***</a>
+        <a className="SectionMenu" onClick={handleToggleBox}>
+          ***
+        </a>
+        {showBox && (
+          <div className="overlay" onClick={handleToggleBox}>
+            <div className="boxContent">
+              <div className="itemOption">
+                <FontAwesomeIcon icon={faPenToSquare} />
+                <p className="textItem">Edit section</p>
+              </div>
+              <div className="itemOption">
+                <FontAwesomeIcon icon={faCopy} />
+                <p className="textItem">Duplicate section</p>
+              </div>
+              <div className="itemOption">
+                <FontAwesomeIcon icon={faTrash} />
+                <p className="textItem">Delete section</p>
+              </div>
+              {/* Các phần tử <a> khác */}
+            </div>
+          </div>
+        )}
       </div>
       <div className="addTask">
         <FontAwesomeIcon icon={faPlus} />
